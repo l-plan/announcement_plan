2f9943c7a7c689a382b7b1b6bf19617721c1d305
rolf <rolf@l-plan.nl>
2015-09-23 23:25:05 +0200

    more scopes, more tests

--- app/models/announcement_plan/announcement.rb
+++ app/models/announcement_plan/announcement.rb
@@ -12,20 +12,39 @@ module AnnouncementPlan
     scope :as_role, -> (user) {joins(:roles).where("announcement_plan_roles.name = ?", user.role.naam ) }
     scope :as_addressee, -> (user) {joins(:addressees).where("announcement_plan_addressees.user_id = ?", user.id ) }
 
-    scope :unread, -> (user){joins(:recipients).where.not("announcement_plan_recipients.user_id = ?", user.id )}
+    # scope :unread, -> (user){where(nil).joins(:recipients).where.not("announcement_plan_recipients.user_id = ?", user.id )}
+    # scope :unread, -> (user){where(nil).includes(:recipients)}
+       # scope :unread, -> (user){where(nil).joins(:recipients).where( "announcement_plan_recipients.id = ?", nil) }
+    # Friend.includes(:contacts).where( :contacts => { :id => nil } )
+    # scope :without_photos, joins('LEFT OUTER JOIN photos ON cities.id = photos.city_id').group('cities.id').having('count(photos.id) = 0')
+    # scope :unread, joins('LEFT OUTER JOIN photos ON cities.id = photos.city_id').group('cities.id').having('count(photos.id) = 0')
 
-    scope :current, -> (time = Time.now ) { where("start_announcing_at < :start AND stop_announcing_at > :stop", start: time, stop: time ) }
+
+    # City.includes(:photos).where.not( photos: {city_id: nil} )
+    # includes(:recipients).where.not( recipients: {announcement_id: nil} )
+    # scope :unread, -> (user){includes(:recipients).where.not( "announcement_plan_recipients.announcement_id = ?", nil) }
+        # includes(:recipients).where.not( recipients: {announcement_id: nil} )
+    # scope :unread, -> (user){includes(:recipients).where.not( recipients: {id: nil}).references(:recipients) }
+
+    scope :unread, -> (user){ where(id: ( where(nil) - joins(:recipients).where("announcement_plan_recipients.user_id= ?",user.id) ).map(&:id).uniq  )}
+    scope :read, -> (user){ joins(:recipients).where("announcement_plan_recipients.user_id= ?",user.id) }
+    # AnnouncementPlan::Announcement.joins(:recipients).where("announcement_plan_recipients.user_id= ?",2).size
+
+    scope :current, -> (time = Time.now ) { where("start_announcing_at < :time AND stop_announcing_at > :time", time: time ) }
 
 
-    scope :for_user, -> (user) { where(nil)}
+  
 
-    def self.for_user(user)
+    def self.for_user(user,time)
+      time =Time.now
+      rel = []
+      rel += as_tenant(user).map(&:id) if AnnouncementPlan.configuration.with_tenant
+      rel += as_role(user).map(&:id) if AnnouncementPlan.configuration.with_role
+      rel += as_addressee(user).map(&:id)
+      rel = rel.uniq
+      rel -= read(user)
 
-      rel = current.unread(user)
-      rel = rel.as_tenant(user) if AnnouncementPlan.configuration.with_tenant
-      rel = rel.as_role(user) if AnnouncementPlan.configuration.with_role
-      rel = rel.as_addressee(user)
-      rel
+      where(id: rel).current(time)
 
     end
 
--- test/models/announcement_plan/announcement_test.rb
+++ test/models/announcement_plan/announcement_test.rb
@@ -24,45 +24,33 @@ module AnnouncementPlan
 
     test "current amount in testset-with-time" do
       Time.stub :now, Time.new(2014,9,16) do 
-        Announcement.current( Time.new(2015,9,17) ).size.must_equal 2
+        Announcement.current( Time.new(2015,9,17) ).size.must_equal 8
       end
 
     end
-    test "2current amount in testset" do
-      # assert @announcement.users.size = 4
-      assert_equal 1, @announcement.addressees.size
 
+    test "amount of addressees for announcement #1" do
+     @announcement.addressees.size.must_equal 3
     end
 
-    test "the truth again" do
-      # assert @announcement.users.size = 4
-      assert_equal 1, @announcement.class.as_addressee(@user).size
-
+    test "amount of announcements for user #1 as just an user" do
+      @announcement.class.as_addressee(@user).size.must_equal 2
     end
 
+    test "amount of announcements for user #1 as an user with a role" do
+      #user 1 is a employee. there are 2 announcements for employees
+      @announcement.class.as_role(@user).size.must_equal 2
+    end
 
-  test "test unscoped op car-source" do
-      Announcement.unscoped.size.must_equal 10
-      # assert_equal 10, Announcement.unscoped.size
-      # "A".downcase.must_equal 'b'
-  end
+    test "amount of announcements for user #1 as an user within a tenant" do
+      #user 1 is in tenant #1 there are 2 announcements for users of this tenant
+      @announcement.class.as_tenant(@user).size.must_equal 2
+    end
 
+    test "amount of announcements for user #1 as an user within a tenant, as an user or wiht his role" do
+      #user 1 is in tenant #1 there are 2 announcements for users of this tenant
+      @announcement.class.for_user(@user, nil).size.must_equal 5
+    end
 
-  	 # let(:announcement) { }
-    # test "the truth" do
-    # 	# binding.pry
-    #  #  @announcement.users.size.must_equal 13
-    #  #  @announcement.users.size
-    #  'a'.upcase.must_equal 'B'
-    # end
-
-
-
-# describe "when asked about integration-testing" do
-    # test "test_the_obvious" do
-    # 	'a'.upcase.must_equal 'B'
-    # 	# @announcement.users.size.must_equal 13
-    # end
-# end
   end
 end
